7d8ce6276c4ab17e89b0f1a9dedf8b09faa5b56e
Kanha Kumar Khatua <kanha@rixosys.com>
2024-04-02 12:16:44 +0530

    For show subscription modal

--- components/Library.tsx
+++ components/Library.tsx
@@ -8,6 +8,7 @@ import useUploadModal from '@/hooks/useUploadModal';
 import { Song } from '@/types';
 import MediaItem from './MediaItem';
 import useOnPlay from '@/hooks/useOnPlay';
+import useSubscribeModal from '@/hooks/useSubscribeModal';
 
 interface LibraryProps {
     songs: Song[]
@@ -16,10 +17,10 @@ interface LibraryProps {
 const Library:React.FC<LibraryProps> = ({
     songs
 }) => {
-
+    const subscribeModal = useSubscribeModal();
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
-    const {user} = useUser();
+    const {user, subscription} = useUser();
 
     const onPlay = useOnPlay(songs);
 
@@ -27,6 +28,12 @@ const Library:React.FC<LibraryProps> = ({
         if(!user){
             return authModal.onOpen();
         }
+        
+        // For show subscription modal
+        if(!subscription){
+            subscribeModal.onOpen()
+        }
+
         return uploadModal.onOpen();
     }
     return (
--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -2,14 +2,21 @@ import { Song } from "@/types";
 import useAuthModal from "./useAuthModal";
 import usePlayer from "./usePlayer"
 import { useUser } from "./useUser";
+import useSubscribeModal from "./useSubscribeModal";
 
 const useOnPlay = (songs: Song[]) => {
     const player = usePlayer();
     const authModal = useAuthModal();
-    const { user } = useUser();
+    const subscribeModal = useSubscribeModal();
+    const { user, subscription } = useUser();
 
     const onplay = (id: string) => {
         if(!user) { return authModal.onOpen(); }
+
+        // For show subscription modal
+        if(!subscription){
+            subscribeModal.onOpen()
+        }
 
         player.setId(id);
         player.setIds(songs.map((song)=> song.id));
